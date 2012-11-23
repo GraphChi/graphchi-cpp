786b6f6551df90b2a86a6786d464a4c2e123e987
Danny Bickson <danny.bickson@gmail.com>
2012-11-23 10:57:01 -0500

    improved visualization website

---
+++ toolkits/visual/refresh_website.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#change the website path to point to your website dir, and then run "sh refresh_website.sh" when you 
+#want to view your graph
+cp index.html *.css *.js graph1000.csv logo.gif settingA /afs/cs.cmu.edu/user/bickson/www/graphchi/
