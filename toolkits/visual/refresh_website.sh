03a2c737720e1dc48aa96eb3f0bc94f0e1a651d4
Danny Bickson <danny.bickson@gmail.com>
2012-11-25 02:57:46 -0500

    added file listing web file

--- toolkits/visual/refresh_website.sh
+++ toolkits/visual/refresh_website.sh
@@ -2,4 +2,4 @@
 
 #change the website path to point to your website dir, and then run "sh refresh_website.sh" when you 
 #want to view your graph
-cp index.html *.css *.js graph1000.csv logo.gif settingA /afs/cs.cmu.edu/user/bickson/www/graphchi/
+cp index.html *.css *.js graph1000.csv logo.gif settingA file.txt /afs/cs.cmu.edu/user/bickson/www/graphchi/
