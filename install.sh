75dfa0572cadb55bb5d4ab8ff61cf8b2bc7ce847
Danny Bickson <danny.bickson@gmail.com>
2012-12-14 12:24:02 -0500

    fixed install script for eigen - thnks to brad cox

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #written by Danny Bickson, CMU
 wget http://bitbucket.org/eigen/eigen/get/3.1.1.tar.bz2
 tar -xjf 3.1.1.tar.bz2
-mv eigen-eigen-43d9075b23ef/Eigen ./src
+mv eigen-eigen-*/Eigen ./src
 cd toolkits/collaborative_filtering
 make 
 cd ../../
