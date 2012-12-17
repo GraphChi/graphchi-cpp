ab82e1443e0d85a0f64f73de67e6a7c99a202a27
Danny Bickson <danny.bickson@gmail.com>
2012-12-17 15:49:37 -0500

    optimizing sparse_gensgd for kddb dataset

--- toolkits/collaborative_filtering/sparse_gensgd.cpp
+++ toolkits/collaborative_filtering/sparse_gensgd.cpp
@@ -41,7 +41,7 @@
 #include "../parsers/common.hpp"
 #include <omp.h>
 #define MAX_FEATAURES 256
-#define FEATURE_WIDTH 17 //MAX NUMBER OF ALLOWED FEATURES IN TEXT FILE
+#define FEATURE_WIDTH 24 //MAX NUMBER OF ALLOWED FEATURES IN TEXT FILE
 
 double gensgd_rate1 = 1e-02;
 double gensgd_rate2 = 1e-02;
