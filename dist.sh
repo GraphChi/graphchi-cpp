a705e039357edab67242891269905135e74155e2
Aapo Kyrola <akyrolacmu@gmail.com>
2013-01-21 11:22:03 -0500

    removed xcode build

--- dist.sh
+++ dist.sh
@@ -2,10 +2,12 @@
 #!/bin/sh
 mkdir dist
 mkdir dist/graphchi_v$1
-cp -r src example_apps toolkits conf docs README.txt graphchi_xcode Makefile dist/graphchi_v$1
-cd dist
+
+cd dist/graphchi_v$1
+hg clone https://code.google.com/p/graphchi/ 
 #rm -rf  src/Eigen
 find . -name '*.hg' -exec rm -rf {} \;
 find . -name '*.dSYM' -exec rm -fr {} \;
+cd ..
 
 tar -czf graphchi_src_v$1.tar.gz graphchi_v$1
