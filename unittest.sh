544b497a0674d3000169ee882c892c658459872f
Danny Bickson <danny.bickson@gmail.com>
2013-01-24 08:59:17 -0500

    added quiet flag to unit test to reduce output

--- unittest.sh
+++ unittest.sh
@@ -71,6 +71,6 @@ rm -fR ./toolkits/collaborative_filtering/unittest/itemsim2rating.unittest.graph
 diff ./toolkits/collaborative_filtering/unittest/itemsim2rating.unittest.graph-rec ./toolkits/collaborative_filtering/unittest/itemsim2rating.unittest
 display_name "TESTING ITEMCF - CORRECTNESS"
 rm -fR ./toolkits/collaborative_filtering/unittest/itemcf.unittest.graph.*
-./toolkits/collaborative_filtering/itemcf --training=./toolkits/collaborative_filtering/unittest/itemcf.unittest.graph --min_allowed_intersection=2 --K=5 --nshards=1 --debug=1 execthreads 1
+./toolkits/collaborative_filtering/itemcf --training=./toolkits/collaborative_filtering/unittest/itemcf.unittest.graph --min_allowed_intersection=2 --K=5 --nshards=1 --quiet=1 execthreads 1
 sh ./toolkits/collaborative_filtering/topk.sh ./toolkits/collaborative_filtering/unittest/itemcf.unittest.graph
 diff ./toolkits/collaborative_filtering/unittest/itemcf.unittest.graph-topk ./toolkits/collaborative_filtering/unittest/itemcf.unittest.graph-topk-correct
