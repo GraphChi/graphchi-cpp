621eefd21578441b3f5d6b20eee5a4d609d57ec6
Danny Bickson <danny.bickson@gmail.com>
2013-02-03 13:39:15 -0500

    fixed itemcf3

--- unittest.sh
+++ unittest.sh
@@ -82,4 +82,6 @@ display_name "MAP METRIC - test 2"
 display_name "TOP K"
 ./toolkits/parsers/topk --training=./toolkits/collaborative_filtering/unittest/topk.unittest --K=3 --quiet=1
 diff ./toolkits/collaborative_filtering/unittest/topk.unittest.ids ./toolkits/collaborative_filtering/unittest/topk.unittest.ids.correct
-
+display_name "ITEMCF3"
+./toolkits/collaborative_filtering/itemcf3 --training=/toolkits/collaborative_filtering/unittest/itemcf3.unittest.graph --distance=9 --debug=0 --quiet=1 --execthreads=1 
+diff ./toolkits/collaborative_filtering/unittest/itemcf3.unittest.correct ./toolkits/collaborative_filtering/unittest/itemcf.unittest.graph.out0
