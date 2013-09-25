b2752412d9617a14fbd8cfad811717bbfe2791f5
Aapo Kyrola <aapo@protogeo.com>
2013-09-25 12:38:34 -0700

    added contraction app

---
+++ graphchi_xcode/contractionresearch/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  contractionresearch
+//
+//  Created by Aapo Kyrola on 9/25/13.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
