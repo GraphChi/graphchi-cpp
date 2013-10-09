47b054e7c745c52c09bf924f491b2a7c993bad89
Aapo Kyrola <aapo@protogeo.com>
2013-10-09 16:07:15 -0700

    removed obsolete call

--- toolkits/graph_analytics/bond_percolation.cpp
+++ toolkits/graph_analytics/bond_percolation.cpp
@@ -188,7 +188,6 @@ int main(int argc, const char ** argv) {
   engine.set_enable_deterministic_parallelism(false);
   engine.set_modifies_inedges(false);
   engine.set_modifies_outedges(false);
-  engine.set_preload_commit(false);
   engine.set_maxwindow(engine.num_vertices());
 
   mytimer.start();
